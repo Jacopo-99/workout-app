794416e0d1b3ef182e73dc54d8522e8311317a70
jacopo <jacopo.loprete.99@gmail.com>
2025-04-05 19:53:59 +0700

    Fix: Render deployment config

--- app.js
+++ app.js
@@ -404,6 +404,7 @@ app.delete('/workouts/:id', (req, res) => {
 });
 
 // AVVIO SERVER
-app.listen(3000, () => {
-  console.log('Server avviato su http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server avviato su http://localhost:${PORT}`);
 });
