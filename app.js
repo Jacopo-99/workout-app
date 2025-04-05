c6f8b5cc0eb63d6d85eca9b41f95d79559f07c85
jacopo <jacopo.loprete.99@gmail.com>
2025-04-05 20:13:27 +0700

    Fix typo in PORT definition

--- app.js
+++ app.js
@@ -404,7 +404,7 @@ app.delete('/workouts/:id', (req, res) => {
 });
 
 // AVVIO SERVER
-cconst PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server avviato su http://localhost:${PORT}`);
 });
